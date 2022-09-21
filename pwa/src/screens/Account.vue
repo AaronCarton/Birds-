0509799c7b6523a687a6a7d3bceea2c4235642c6
Aaron <aaron.carton@student.howest.be>
2022-09-21 13:54:57 +0200

    update

---
+++ pwa/src/screens/Account.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Hi, {{ user?.displayName }}</h1>
+</template>
+
+<script setup lang="ts">
+import useAuthentication from '../composables/useAuthentication'
+
+const { user } = useAuthentication()
+</script>
