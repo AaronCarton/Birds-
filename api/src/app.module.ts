c9e2732bfd400998556dafb4576d9065f020a834
Aaron <aaron.carton@student.howest.be>
2022-09-27 12:35:57 +0200

    typeORM & graphQL setup

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ApolloDriverConfig } from '@nestjs/apollo'
+import { graphqlConfig } from './bootstrap/graphQLConfig'
+import { typeORMConfig } from './bootstrap/typeORMConfig'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    GraphQLModule.forRoot<ApolloDriverConfig>(graphqlConfig),
+  ], // TODO: move to async provider
   controllers: [],
   providers: [],
 })
