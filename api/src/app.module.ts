4daed6774f8bc0c2c695d56ded875e5730402d77
Aaron <aaron.carton@student.howest.be>
2022-10-03 14:37:23 +0200

    add database seeding command to nestjs

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,6 +7,7 @@ import { typeORMConfig } from './bootstrap/typeORMConfig'
 import { BirdsModule } from './resources/birds/birds.module'
 import { AreasModule } from './resources/areas/areas.module'
 import { ObservationsModule } from './resources/observations/observations.module'
+import { DatabaseSeedModule } from './seeds/seed.module'
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { ObservationsModule } from './resources/observations/observations.module
     BirdsModule,
     AreasModule,
     ObservationsModule,
+    DatabaseSeedModule,
   ], // TODO: move to async provider
   controllers: [],
   providers: [],
